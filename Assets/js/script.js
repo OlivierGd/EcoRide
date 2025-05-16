b42609c951fdd1523f41612262883095343b519a
OlivierGd <oguissard@yahoo.fr>
2025-05-16 08:54:31 +0200

    Ajout du js pour le formulaire de recherche d'une destination

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,28 @@
-//add date into the form//
+//Search form destination
+// Waitting DOM is completly charged
 document.addEventListener('DOMContentLoaded', () => {
-    const dateInput = document.getElementById('formSearchDate');
+    const form = document.getElementById('formSearchDestination');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const depart = document.getElementById('searchStartCity').value.trim();
+        const destination = document.getElementById('searchEndCity').value.trim();
+        const date = document.getElementById('searchDate').value;
+
+        if (!depart || !destination || !date) {
+            alert('Merci de compléter les champs : Départ, Destination et date');
+            return;
+        }
+
+        console.log(`Départ : ${depart}`);
+        console.log(`Destination: ${destination}`);
+        console.log(`Date : ${date}`);
+    })
+})
+
+
+//add day date of today into the form when charging page//
+document.addEventListener('DOMContentLoaded', () => {
+    const dateInput = document.getElementById('searchDate');
     if (dateInput) {
         dateInput.value = getTodayDate();
     }
