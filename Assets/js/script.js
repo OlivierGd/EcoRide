9528ca8be9a592b4ee6edc782baf9d9211222a76
OlivierGd <oguissard@yahoo.fr>
2025-05-18 15:27:40 +0200

    commit before deploying on heroku

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,21 @@
 //Search form destination
 // Waitting DOM is completly charged
 document.addEventListener('DOMContentLoaded', () => {
+    //Rechercher le formulaire
     const form = document.getElementById('formSearchDestination');
+    //Soumettre le formulaire
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+        //Récupérer les valeurs des champs saisis
         const depart = document.getElementById('searchStartCity').value.trim();
         const destination = document.getElementById('searchEndCity').value.trim();
         const date = document.getElementById('searchDate').value;
-
+        //Afficher un message d'alerte si un champ est vide
         if (!depart || !destination || !date) {
             alert('Merci de compléter les champs : Départ, Destination et date');
             return;
         }
-
+        //Vérifier dans la console le résultat
         console.log(`Départ : ${depart}`);
         console.log(`Destination: ${destination}`);
         console.log(`Date : ${date}`);
