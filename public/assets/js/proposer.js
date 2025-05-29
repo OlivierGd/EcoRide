9ae95e0195f6ab0002af8fa21485baf3d9403fc2
OlivierGd <oguissard@yahoo.fr>
2025-05-29 16:36:36 +0200

    Eco-92 - Ajout de la modale de confirmation pour la validation du formulaire de proposition de trajet

--- public/assets/js/proposer.js
+++ public/assets/js/proposer.js
@@ -3,7 +3,7 @@ function getSelectedPlace() {
     const places = document.querySelectorAll('input[name="places"]');
     for (const place of places) {
         if (place.checked) {
-            return place.value;
+            return parseInt(place.value); // converti le string en num pour le nombre de places
         }
     }
     return "0";
@@ -11,8 +11,8 @@ function getSelectedPlace() {
 // Fonction pour récupérer le prix proposé par passager
 function getPriceSuggested() {
     const priceInput = document.getElementById('priceRequested');
-    const price = parseFloat(priceInput.value);
-    return price;
+    const price = parseInt(priceInput.value);
+    return parseInt(priceInput.value);
 }
 
 //fonction pour mettre à jour l'affichage du nombre de places
@@ -38,30 +38,58 @@ document.getElementById('priceRequested').addEventListener('input', updateDispla
 //Affiche la valeur du prix selon le nombre de places sélectionnées
 document.addEventListener('DOMContentLoaded', updateDisplay);
 
+
 //Fonction pour récupérer les données du formulaire
 
 const dataSuggestedForm = [];
-function getDataSuggestedForm() {
-    event.preventDefault(); //Empeche le rechargement du formaulaire
+function getDataSuggestedForm(e) {
+    e.preventDefault(); //Empeche le rechargement du formulaire
 
     const inputStartCity = document.getElementById('suggestedStartCity');
     const inputEndCity = document.getElementById('suggestedEndCity');
+    const inputStartDate = document.getElementById('proposalDate');
+    const inputStartTime = document.getElementById('proposalTime');
+    const inputPlaceAvailable = getSelectedPlace();
+    const inputProposalCredits = document.getElementById('priceRequested');
 
     const startCity = inputStartCity.value.trim();
     const endCity = inputEndCity.value.trim();
-    console.log(startCity, endCity);
+    const startDate = inputStartDate.value.trim();
+    const startTime = inputStartTime.value.trim();
+    const placeAvailable = getSelectedPlace();
+    const proposalCredits = parseInt(inputProposalCredits.value.trim());
+    console.log(startCity, endCity, startDate, startTime, placeAvailable, proposalCredits);
 
-    if (!startCity || !endCity) {  //Vérifie si les champs sont complétés
+    if (!startCity || !endCity || !startDate || !startTime || !placeAvailable || !proposalCredits) {  //Vérifie si les champs sont complétés
         console.warn("Veuillez remplir tous les champs !");
         return;
     }
+
+    // Afficher la modale de confirmation des données du formulaire
+    const modal = document.getElementById('confirmationModal');
+    const modalText = document.getElementById('modalText');
+    modalText.innerHTML = `Vous proposez un trajet de <strong>${startCity} </strong> à <strong>${endCity} </strong>le <strong>${startDate}</strong>. Confirmez-vous ?`;
+    const modalInstance = new bootstrap.Modal(modal);
+    modalInstance.show();
+
+    // Réponse et validation de la confirmation
+    document.getElementById('confirmSubmit').onclick = () => {
+        const modaleValidation = bootstrap.Modal.getInstance(modal);
+        modaleValidation.hide();// Ferme la modale
+    }
+
     const formData = {
         startCity: startCity,
         endCity: endCity,
+        startDate: startDate,
+        startTime: startTime,
+        placeAvailable: placeAvailable,
+        proposalCredits: proposalCredits,
     }
     dataSuggestedForm.push(formData);
     console.log("Données du formulaire : ",formData);
     console.log("Tableau complet : ", dataSuggestedForm);
+
 }
 document.addEventListener('DOMContentLoaded', () => {
     const publishButton = document.getElementById('publishSuggestedForm');
