33a2f18422120aeb03f8b97e77c5ce2284df9399
OliG <oguissard@yahoo.fr>
2025-09-08 22:40:05 +0200

    modification logo

--- public/vehicule.php
+++ public/vehicule.php
@@ -80,7 +80,7 @@ $pageTitle = 'Mes véhicules';
         <nav class="navbar bg-body-tertiary">
             <div class="container" style="max-width: 900px;">
                 <a class="navbar-brand" href="index.php">
-                    <img src="assets/pictures/logoEcoRide.webp" alt="Logo EcoRide" width="60" class="d-inline-block align-text-center rounded">
+                    <img src="assets/pictures/logoEcoRide.png" alt="Logo EcoRide" width="60" class="d-inline-block align-text-center rounded">
                 </a>
                 <h2 class="fw-bold mb-1 text-success">Mes véhicules</h2>
                 <?= displayInitialsButton(); ?>
